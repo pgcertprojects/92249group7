c3fea82c9a6d0e0ea51ad8b5fefb0dd89bd635ca
Michael_Love <mike@michaeljlove.co.uk>
2021-02-11 20:12:56 +0000

    Thursday night

---
+++ src/com/User.java
@@ -0,0 +1,15 @@
+package com;
+
+/**
+ * Created by mike on 11/02/2021
+ * The user superclass
+ **/
+public class User
+{
+
+   public static void main(String[] args)
+   {
+
+
+   } //main
+} //class
