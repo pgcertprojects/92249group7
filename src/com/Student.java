44923b15e8c509c2e704866cd676226fa29fc393
Michael_Love <mike@michaeljlove.co.uk>
2021-02-11 20:54:09 +0000

    Thursday night again

---
+++ src/com/Student.java
@@ -0,0 +1,15 @@
+package com;
+
+/**
+ * Created by mike on 11/02/2021
+ * The Student file
+ **/
+public class Student
+{
+
+   public static void main(String[] args)
+   {
+
+
+   } //main
+} //class
